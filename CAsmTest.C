00269fdb09e361668ee2b4ec4c159f5dfccb451b
InsaneMiner <gurued58@gmail.com>
2021-05-19 01:05:07 +0200

    Fixed it so that i could compile with gcc

--- CAsmTest.C
+++ CAsmTest.C
@@ -6,7 +6,7 @@ int main(){
  U8 bin[255]={0};
  U8 len=0;
  U32 args[]={C_MNM_PUSH,C_OPT_REG,C_REG_FLAGS};
- CAsm(C_ARC_AMD64,C_SET_UNMANG,bin,&len,&args);
+ CAsm(C_ARC_AMD64,C_SET_UNMANG,bin,&len,(U32*)&args);
  for(U32 i=0;i<len;++i){
   printf("%02X ",bin[i]);
  }
